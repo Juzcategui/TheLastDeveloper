e8952c2b6fe777d0768a9e78dcd510637688ec29
gbaek3 <g.baek24@gmail.com>
2019-03-23 18:07:25 -0400

    basic comps and routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from "react-router-dom";
+import Login from './components/Login';
+import Town from './components/Town';
+import Forest from './components/Forest';
+import HTMLHouse from './components/Houses/HTMLHouse';
+import JavascriptHouse from './components/Houses/JavascriptHouse';
+import ReactHouse from './components/Houses/ReactHouse';
+import CSSHouse from './components/Houses/CSSHouse';
 import World from './Features/World';
 
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <World />
-      </div>
-    )
-  }
-}
+const App = () => (
+  <BrowserRouter>
+    <div>
+      <Route exact path="/" component={Login} />
+      <Route exact path="/Town" component={Town} />
+      <Route exact path="/Forest" component={Forest} />
+      <Route exact path="/HTMLHouse" component={HTMLHouse} />
+      <Route exact path="/CSSHouse" component={CSSHouse} />
+      <Route exact path="/JavascriptHouse" component={JavascriptHouse} />
+      <Route exact path="/ReactHouse" component={ReactHouse} />
+      <Route exact path="/world" component={World} />
+    </div>
+  </BrowserRouter>
+)
 
 export default App;
