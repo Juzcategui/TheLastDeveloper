e4dedb514db4ecd8590d3474f0035f41bb5030c3
Son <sonbui.inbox@gmail.com>
2019-03-26 00:59:53 -0400

    CSS/HTML/WorldMap update

--- client/src/components/Technical/WorldMap.js
+++ client/src/components/Technical/WorldMap.js
@@ -11,21 +11,43 @@ class WorldMap extends React.Component {
     render() {
         return (
             <div>
+                <h1 id="WorldMapTitle">WORLD MAP</h1>
                 <img class="BG" src="https://www.creativeuncut.com/gallery-17/art/com-world-map.jpg" alt="WorldMap" />
 
-                <Link to="/JavascriptTown">Javascript Junkies</Link>
-                <br />
-                <Link to="/ReactTown">Rivers of React</Link>
-                <br />
-                <Link to="/HTMLTown">House of HTML</Link>
-                <br />
-                <Link to="/CSSTown">CSS Cults</Link>
-                <br />
-                <Link to="/OuterTown">Forest</Link>
-                <br />
-                <Link to="/SecretCave">Secret Cave</Link>
-                <br />
-                <Link to="/WillBucks">WillBucks</Link>
+                <h3 class="JSWorldTitle">Javascript Junkies</h3>
+                <Link to="/JavascriptTown">
+                    <img id="JavascriptJunkies" src="http://i.imgur.com/hgvDp.gif"></img>
+                </Link>
+
+                <h3 class="ReactWorldTitle">Rivers of React</h3>
+                <Link to="/ReactTown">
+                <img id="RiversOfReact" src="http://i.imgur.com/hgvDp.gif"></img>
+                </Link>
+
+                <h3 class="HTMLWorldTitle">House of HTML</h3>
+                <Link to="/HTMLTown">
+                <img id="HouseOfHTML" src="http://i.imgur.com/hgvDp.gif"></img>
+                </Link>
+
+                <h3 class="CSSWorldTitle">CSS Cult</h3>
+                <Link to="/CSSTown">
+                    <img id="CSSCult" src="http://i.imgur.com/hgvDp.gif"></img>
+                </Link>
+
+                <h3 class="ForestWorldTitle">Forest</h3>
+                <Link to="/OuterTown">
+                    <img id="ForestWorldMap" src="http://i.imgur.com/hgvDp.gif"></img>
+                </Link>
+
+                <h3 class="SecretCaveWorldTitle">???</h3>
+                <Link to="/SecretCave">
+                    <img id="SecretCaveWorldMap" src="http://i.imgur.com/hgvDp.gif"></img>
+                </Link>
+
+                <h3 class="WillBucksWorldTitle">WillBucks</h3>
+                <Link to="/WillBucks">
+                    <img id="WillBucksWorldMap" src="http://i.imgur.com/hgvDp.gif"></img>
+                </Link>
             </div>
         )
     }
