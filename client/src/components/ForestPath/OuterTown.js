ad1ac678604447062132eaccf609f57526fcb1c4
gbaek3 <g.baek24@gmail.com>
2019-03-27 20:49:24 -0400

    question logic

--- client/src/components/ForestPath/OuterTown.js
+++ client/src/components/ForestPath/OuterTown.js
@@ -16,22 +16,26 @@ class OuterTown extends React.Component {
                 <h1 className="ForestTitles">Outer Town</h1>
                 <img className="BG" src={OuterTownBG} alt="OuterTown" />
 
-                    <div id='DialogContainer'>
+                <div id='DialogContainer'>
 
-                        <div id="characterName">
-                            <p>Insert character name here</p>
-                        </div>
-
-                        <div id="textBox">
-                            <h3>Theres something that smells nearby, but you realized it was just you.</h3>
-                            <p>Ya piece of garbage</p>
-                        </div>
+                    <div id="characterName">
+                        <p>Insert character name here</p>
                     </div>
 
-                    <div id="actionBox">
-                        <button className="btn btn-success"><Link to="/OuterForest">Continue to forest</Link></button>
-                        <button className="btn btn-secondary"><Link to="/WorldMap">World Map</Link></button>
+                    <div id="textBox">
+                        <h3>Theres something that smells nearby, but you realized it was just you.</h3>
+                        <p>Ya piece of garbage</p>
                     </div>
+                </div>
+
+                <div id="actionBox">
+                    <Link to="/OuterForest">
+                        <button className="btn btn-success">Continue to forest</button>
+                    </Link>
+                    <Link to="/WorldMap">
+                        <button className="btn btn-secondary">World Map</button>
+                    </Link>
+                </div>
 
             </div>
         )
