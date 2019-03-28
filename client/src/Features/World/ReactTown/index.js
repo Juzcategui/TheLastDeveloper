6ae4f757b12d5968051c9b9cef2cb2e70362c822
gbaek3 <g.baek24@gmail.com>
2019-03-28 14:58:23 -0400

    movement

--- client/src/Features/World/ReactTown/index.js
+++ client/src/Features/World/ReactTown/index.js
@@ -2,32 +2,34 @@ import React from 'react';
 import Map from '../../Map'
 import Player from '../../Player/';
 import { Link } from "react-router-dom";
-import worldBG from '../../../Backgrounds/VermillionCity.png'
+import worldBG from '../../../Backgrounds/PewterCity.png'
 
 import { ReactTiles } from './ReactTiles'
 import store from '../../../config/store'
 
 
 function ReactTown(props) {
-    store.dispatch({ type: 'ADD_TILES', payload: {
-        tiles: ReactTiles,
-        name: "React Town"
-    }})
-    
+    store.dispatch({
+        type: 'ADD_TILES', payload: {
+            tiles: ReactTiles,
+            name: "React Town"
+        }
+    })
+
     return (
 
         <div
-        style={{
-            position: 'relative',
-            width: '1280px',
-            height: '720px',
-            margin: '20px auto',
-            backgroundImage: `url('${worldBG}')`
-        }}>
-        <Map tiles={ReactTiles} name="React Town"/>
-        <Player />
+            style={{
+                position: 'relative',
+                width: '1280px',
+                height: '720px',
+                margin: '20px auto',
+                backgroundImage: `url('${worldBG}')`
+            }}>
+            <Map tiles={ReactTiles} name="React Town" />
+            <Player />
 
-            
+
         </div>
     )
 }
