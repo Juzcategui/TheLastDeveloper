af02f7bda912b0842936a1cad1b1f1afab52db7a
Chaiku <46286216+Chaiku@users.noreply.github.com>
2019-03-29 11:10:53 -0400

    about to pull master again

--- client/src/Features/World/HTMLTown/index.js
+++ client/src/Features/World/HTMLTown/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Map from '../../Map'
 import Player from '../../Player/';
-import { Link } from "react-router-dom";
 import worldBG from '../../../Backgrounds/VermillionCity.png'
 import HTMLNPC from './HTML_NPC'
 import RedRedWine from "../../../Audio/RedRedWine.mp3"
@@ -32,11 +31,7 @@ function HTMLTown(props) {
             <Map tiles={HTMLTiles} name="HTML Town" />
             <Player />
             <HTMLNPC />
-<<<<<<< HEAD
-            
-=======
             <audio src={RedRedWine} autoPlay/>
->>>>>>> 4cd8b32204e0045c39faaeed392378f3c175033a
 
 
         </div>
