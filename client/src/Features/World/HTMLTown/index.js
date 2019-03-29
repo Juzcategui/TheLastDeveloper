b041e0546930243893524be4846320fd6ddb8463
vupnguyen55 <vupnguyen55@gmail.com>
2019-03-28 20:44:53 -0400

    jorge audio

--- client/src/Features/World/HTMLTown/index.js
+++ client/src/Features/World/HTMLTown/index.js
@@ -4,7 +4,7 @@ import Player from '../../Player/';
 import { Link } from "react-router-dom";
 import worldBG from '../../../Backgrounds/VermillionCity.png'
 import HTMLNPC from './HTML_NPC'
-import RedRedWine from "../../Audio/RedRedWine.mp3"
+import RedRedWine from "../../../Audio/RedRedWine.mp3"
 
 import { HTMLTiles } from './HTMLTiles'
 import store from '../../../config/store'
