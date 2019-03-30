dfe24e46537e2c038f09394f526f30bc44e621eb
Son <sonbui.inbox@gmail.com>
2019-03-30 17:56:52 -0400

    master pull

--- client/src/components/ForestPath/Forest.js
+++ client/src/components/ForestPath/Forest.js
@@ -66,7 +66,7 @@ class Forest extends React.Component {
     }
 
     render() {
-        const { battleCount, mongodDialogue, playerDialogue, confirmedClick, buttonDiag } = this.state;
+        const { battleCount, playerDialogue, confirmedClick, buttonDiag } = this.state;
         return (
 
             <div className="wrapper">
